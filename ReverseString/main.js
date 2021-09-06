d18c4ebc3a955e2182e478f5c63eabe2bb595972
Konstantin <65258089+Fellow32@users.noreply.github.com>
2021-09-06 18:23:55 +0300

    Add files via upload

---
+++ ReverseString/main.js
@@ -0,0 +1,7 @@
+// Write a function that reverses a string. The input string is given as an array of characters s.
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
+
+var reverseString = function(s) {
+    return s = s.reverse().join()
+};
